c41b93862fc7946407d85bf8f1492bcfc13b4d9f
Obi Obianom <oobianom@umaryland.edu>
2022-12-06 01:25:12 -0500

    Update sharesocial.js

--- inst/themes/sharesocial.js
+++ inst/themes/sharesocial.js
@@ -1,1 +1,2 @@
+/*! r2social | (c) Written by Obi Obianom, www.obianom.com | all rights reserved */
 $(function(){$(".r2social-link-container a").click(function(t){t.preventDefault(),console.log($(this).attr("href")),window.open($(this).attr("href"),"_r2socialxlink","height=500,width=400,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes")})});
