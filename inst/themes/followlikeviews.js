86d7e5ac92f17b0f338cf0489f26b30f646ca024
Obi Obianom <oobianom@umaryland.edu>
2023-01-01 23:46:04 -0500

    Update followlikeviews.js

--- inst/themes/followlikeviews.js
+++ inst/themes/followlikeviews.js
@@ -13,3 +13,10 @@ $(function() {
      <i class="fa fa-heart" aria-hidden="true"></i> Like it ?
    </button>
 
+$('.r2social-style2-review').on('click', function() {
+  if ($(this).html() == '<i class="fa fa-heart" aria-hidden="true"></i> You liked it!') {
+    $(this).html('<i class="fa fa-heart" aria-hidden="true"></i> Like it ?');
+  } else {
+    $(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked it!');
+  }
+});
