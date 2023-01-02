58ca07d80e73c7e2a7ac3132988ebb2bf4e1a0b4
Obi Obianom <oobianom@umaryland.edu>
2023-01-01 23:43:46 -0500

    Update followlikeviews.js

--- inst/themes/followlikeviews.js
+++ inst/themes/followlikeviews.js
@@ -3,3 +3,13 @@ $(function() {
     $(this).toggleClass("is-active");
   });
 });
+
+
+
+
+
+  <button class="r2social-style2-btn-secondary review" onclick="toggle(this)">
+     <i class="fa fa-heart" aria-hidden="true"></i> Like it ?
+   </button>
+
+
