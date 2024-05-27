05be465469e0e594a4f92c97a8896e08f16460ca
Obi Obianom <oobianom@umaryland.edu>
2024-05-26 20:43:29 -0400

    Update ui.R

--- inst/examples/ui.R
+++ inst/examples/ui.R
@@ -4,6 +4,30 @@ library(r2social)
 
 ui <- fluidPage(
   r2social.scripts(),
+  profileDisplay1(list(
+    a = list(
+      name = "Obi Obianom",
+      title = "Senior Scientist",
+      image = "https://r2social.obi.obianom.com/misc/team3.jpg",
+      social = list(
+        list(name = "x",
+             link = "https://x.com/R2Rpkg"),
+        list(name = "linkedin",
+             link = "https://linkedin.com/oobianom"),
+        list(name = "website",
+             link = "https://obianom.com"),
+        list(name = "youtube",
+             link = "https://youtube.com/R2Rpkg")
+      )
+    ),
+    b = list(
+      name = "William Hane",
+      title = "Core Developer",
+      image = "https://r2social.obi.obianom.com/misc/team1.jpg",
+      social = list(list(name = "youtube",
+                         link = "https://youtube.com/R2Rpkg"))
+    )
+  )),
   shareButton(link = "http://obianom.com", position = "right"),
   shareButton(link = "http://google.com", position = "inline"),
   selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
@@ -13,4 +37,5 @@ ui <- fluidPage(
   connectButton(link = "https://x.com/R2Rpkg", x=TRUE, position = "inline",display.inline = FALSE),
   verbatimTextOutput("summary"),
   tableOutput("table")
+
 )
